6b48f8f185aeba185a6eb04b3581e8cf593e4952
xoxtomon <xoxtomon@hotmail.com>
2021-03-27 16:50:07 -0500

    Reposicionamiento y Escalada
    Se posicionaron los objetos en un nuevo luagar, y se cambiaron los factores de escala.

--- Arbol.cpp
+++ Arbol.cpp
@@ -2,89 +2,109 @@
 
 Arbol::Arbol()
 {
-        if (!objmodel_ptr)
-        {
-            objmodel_ptr = glmReadOBJ("mountain.obj");
-        }
-        if (!objmodel_ptr)
-            exit(0);
+    if (!objmodel_ptr)
+    {
 
-        glmUnitize(objmodel_ptr);
-        glmFacetNormals(objmodel_ptr);
-        glmVertexNormals(objmodel_ptr, 90.0);
+        objmodel_ptr = glmReadOBJ("Mountain.obj");
+    }
+    if (!objmodel_ptr)
+        exit(0);
 
-        if (!objmodel_ptr1)
-        {
+    glmUnitize(objmodel_ptr);
+    glmFacetNormals(objmodel_ptr);
+    glmVertexNormals(objmodel_ptr, 90.0);
 
-            objmodel_ptr1 = glmReadOBJ("nikto.obj");
-        }
-        if (!objmodel_ptr1)
-            exit(0);
+    if (!objmodel_ptr1)
+    {
 
-        glmUnitize(objmodel_ptr1);
-        glmFacetNormals(objmodel_ptr1);
-        glmVertexNormals(objmodel_ptr1, 90.0);
+        objmodel_ptr1 = glmReadOBJ("nikto.obj");
+    }
+    if (!objmodel_ptr1)
+        exit(0);
 
-        if (!objmodel_ptr2)
-        {
+    glmUnitize(objmodel_ptr1);
+    glmFacetNormals(objmodel_ptr1);
+    glmVertexNormals(objmodel_ptr1, 90.0);
 
-            objmodel_ptr2 = glmReadOBJ("tree.obj");
-        }
-        if (!objmodel_ptr2)
-            exit(0);
+    if (!objmodel_ptr2)
+    {
 
-        glmUnitize(objmodel_ptr2);
-        glmFacetNormals(objmodel_ptr2);
-        glmVertexNormals(objmodel_ptr2, 90.0);
+        objmodel_ptr2 = glmReadOBJ("tree.obj");
+    }
+    if (!objmodel_ptr2)
+        exit(0);
 
-        if (!objmodel_ptr3)
-        {
+    glmUnitize(objmodel_ptr2);
+    glmFacetNormals(objmodel_ptr2);
+    glmVertexNormals(objmodel_ptr2, 90.0);
 
-            objmodel_ptr3 = glmReadOBJ("windmill.obj");
-        }
-        if (!objmodel_ptr3)
-            exit(0);
+    if (!objmodel_ptr3)
+    {
 
-        glmUnitize(objmodel_ptr3);
-        glmFacetNormals(objmodel_ptr3);
-        glmVertexNormals(objmodel_ptr3, 90.0);
+        objmodel_ptr3 = glmReadOBJ("windmill.obj");
+    }
+    if (!objmodel_ptr3)
+        exit(0);
 
-        if (!objmodel_ptr4)
-        {
+    glmUnitize(objmodel_ptr3);
+    glmFacetNormals(objmodel_ptr3);
+    glmVertexNormals(objmodel_ptr3, 90.0);
 
-            objmodel_ptr4 = glmReadOBJ("horse.obj");
-        }
-        if (!objmodel_ptr4)
-            exit(0);
+    if (!objmodel_ptr4)
+    {
 
-        glmUnitize(objmodel_ptr4);
-        glmFacetNormals(objmodel_ptr4);
-        glmVertexNormals(objmodel_ptr4, 90.0);
-} 
+        objmodel_ptr4 = glmReadOBJ("horse.obj");
+    }
+    if (!objmodel_ptr4)
+        exit(0);
+
+    glmUnitize(objmodel_ptr4);
+    glmFacetNormals(objmodel_ptr4);
+    glmVertexNormals(objmodel_ptr4, 90.0);
+}
 
 void Arbol::DibujarArbol(int i, float ax, float ay, float az)
 {
+    //MONTANA
     glPushMatrix();
-    glTranslatef(ax, ay, az);
-    glRotatef(90, 0, 1, 0);
-        glmDraw(objmodel_ptr, GLM_SMOOTH | GLM_MATERIAL);
-        glRotatef(-90, 0, 1, 0);
-        glTranslatef(3.0, ay, az);
+        glScalef(10.0f, 10.0f, 10.0f);
+        glTranslatef(ax, ay, -9);
+        //glTranslatef(ax, ay, 3.0);
         glRotatef(90, 0, 1, 0);
+        glRotatef(-90, 1, 0, 0);
+        glScalef(2, 2, 2);
+        glmDraw(objmodel_ptr, GLM_SMOOTH | GLM_MATERIAL);
+    glPopMatrix();
+
+    //CABALLERO
+    glPushMatrix();
+        glScalef(0.4f, 0.4f, 0.4f);
+        glTranslatef(-12.5f, -5.0f, az);
+        glRotatef(-90, 1, 0, 0);
         glmDraw(objmodel_ptr1, GLM_SMOOTH | GLM_MATERIAL);
-        glRotatef(-90, 0, 1, 0);
-        glTranslatef(-6.0, ay, az);
+    glPopMatrix();
+
+    //ARBOL
+    glPushMatrix();
+        glTranslatef(-3.0f, -1.3f, az);
         glmDraw(objmodel_ptr2, GLM_SMOOTH | GLM_MATERIAL);
-        glTranslatef(3.0, -3.0, az);
+    glPopMatrix();
+
+    //MOLINO
+    glPushMatrix();
+        glScalef(1.15f, 1.15f, 1.15f);
+        glTranslatef(-6.0f, -1.25f, az);
         glRotatef(90, 0, 1, 0);
         glmDraw(objmodel_ptr3, GLM_SMOOTH | GLM_MATERIAL);
-        glRotatef(-90, 0, 1, 0);
-        glTranslatef(ax, 6.0, az);
-        glRotatef(90, 0, 1, 0);
-        glmDraw(objmodel_ptr4, GLM_SMOOTH | GLM_MATERIAL);
-        glRotatef(-90, 0, 1, 0);
+    glPopMatrix();
 
-        
+    //CABALLO
+    glPushMatrix();
+        glScalef(0.5f, 0.5f, 0.5f);
+        glTranslatef(-8.25f, -3.7f, az);
+        glRotatef(90, 0, 1, 0);
+        glRotatef(180, 0, 1, 0);
+        glmDraw(objmodel_ptr4, GLM_SMOOTH | GLM_MATERIAL);
     glPopMatrix();
 }
 
